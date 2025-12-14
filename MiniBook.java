f21f15d1bdacff7076c17caa61cef82e6f1cc976
lakiii <luckymaharjan2007@gmail.com>
2025-12-14 19:22:24 +0545

    wORKSHOP6

--- MiniBook.java
+++ MiniBook.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class senario6
+public class MiniBook
 {
     public static void main(String[] args)
     {
